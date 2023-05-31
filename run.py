74dfd8ef4c08fe05a0590ae6b87be6eb95087f96
KaiLangen96 <>
2023-05-31 07:05:24 +0000

    Add random secret_word from word_list and convert into display

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+word_list = ["test", "monkey", "apple"]
+secret_word = random.choice(word_list)
+display = []
+
+for i in range(len(secret_word)):
+    display += '_'
+print(secret_word)
+print(display)
