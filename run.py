f261041c1693b3ebd0d61e169d8018dbff77122f
KaiLangen96 <>
2023-05-31 07:30:30 +0000

    Refactor guessing a letter and adding validation to it

--- run.py
+++ run.py
@@ -9,6 +9,32 @@ secret_word = random.choice(word_list)
 display = []
 lives = 6
 game_over = False
+guessed_letter = ''
+already_used = []
+
+
+def guess_letter():
+    """
+    Getting the guess from the user and checking if
+    the input was valid.
+    """
+    guess_letter = input("Please guess a letter and confirm with Enter: \n")
+    if len(guess_letter) > 1:
+        print("You entered more than one letter!")
+        print("Please enter only one letter and confirm with Enter.")
+        return False
+    if guess_letter.isalpha() is False:
+        print("You have entered a non-alphabetic character.")
+        print("Enter only one alphabetic letter and confirm with Enter.")
+        return False
+    if guess_letter in already_used:
+        print(f"{guess_letter} has already been used.")
+        return False
+
+    guessed_letter = guess_letter.lower()
+    already_used.append(guessed_letter)
+    return guessed_letter
+
 
 if __name__ == "__main__":
     # Creating display with '_' for each letter
@@ -18,7 +44,10 @@ if __name__ == "__main__":
 
     # Hangman game loop
     while not game_over:
-        guessed_letter = input("Guess a letter: ")
+
+        guessed_letter = guess_letter()
+        while guessed_letter is False:
+            guessed_letter = guess_letter()
 
         for position in range(len(secret_word)):
             letter = secret_word[position]
