e2041cf29e0188cd28c2687f090ae9cf4abdec28
KaiLangen96 <>
2023-05-31 07:15:38 +0000

    Add while loop to ask for more inputs and add ways to end the game

--- run.py
+++ run.py
@@ -7,16 +7,31 @@ import hangman_picture
 word_list = ["test", "monkey", "apple"]
 secret_word = random.choice(word_list)
 display = []
+lives = 6
+game_over = False
 
 for i in range(len(secret_word)):
     display += '_'
 print(secret_word)
 print(display)
 
-guessed_letter = input("Guess a letter: ")
+while not game_over:
+    guessed_letter = input("Guess a letter: ")
 
-for position in range(len(secret_word)):
-    letter = secret_word[position]
-    if letter == guessed_letter:
-        display[position] = guessed_letter
-print(display)
+    for position in range(len(secret_word)):
+        letter = secret_word[position]
+        if letter == guessed_letter:
+            display[position] = guessed_letter
+    print(display)
+
+    if guessed_letter not in secret_word:
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print(hangman_picture.lives_left[lives])
+            print("Game over! You lose!")
+
+    if '_' not in display:
+        game_over = True
+        print("Congratulations! You win!")
+    print(hangman_picture.lives_left[lives])
