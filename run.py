1fba83c473eed1ad792a476887afc9f81f5c3484
KaiLangen96 <>
2023-06-01 08:05:23 +0000

    Add colors and space code

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
 import hangman_picture
 import os
@@ -18,8 +15,8 @@ result = ''
 
 def create_display(secret_word):
     """
-    Creates the display string with underscores for each character
-    from secret_word.
+    Creates the display string with underscores for each character from
+    secret_word.
     """
     display = ['_'] * len(secret_word)
     return display
@@ -27,20 +24,23 @@ def create_display(secret_word):
 
 def guess_letter():
     """
-    Getting the guess from the user and checking if
-    the input was valid.
+    Getting the guess from the user and checking if the input was valid.
+    A valid input is only one aplhabetic letter.
     """
-    guess_letter = input("Please guess a letter and confirm with Enter: \n")
+    guess_letter = input("Please guess a letter and confirm with Enter:\n")
+
     if len(guess_letter) > 1:
-        print("You entered more than one letter!")
-        print("Please enter only one letter and confirm with Enter.\n")
+        print("\033[91mYou entered more than one letter!")
+        print("Please enter only one letter.\033[0m\n")
         return False
+
     if guess_letter.isalpha() is False:
-        print("You have entered a non-alphabetic character.")
-        print("Enter only one alphabetic letter and confirm with Enter.\n")
+        print("\033[91mYou have entered a non-alphabetic character.")
+        print("Enter only one alphabetic letter.\033[0m\n")
         return False
+
     if guess_letter in already_used:
-        print(f"{guess_letter} has already been used.")
+        print(f"\033[91m{guess_letter} has already been used.\033[0m")
         return False
 
     guessed_letter = guess_letter.lower()
@@ -54,8 +54,10 @@ def update_display(secret_word, guessed_letter, display):
     """
     for position in range(len(secret_word)):
         letter = secret_word[position]
+
         if letter == guessed_letter:
             display[position] = guessed_letter
+
     return display
 
 
@@ -65,8 +67,10 @@ def check_game(guessed_letter, secret_word, display):
     Returns the game over status.
     """
     global lives, result
+
     if guessed_letter not in secret_word:
         lives -= 1
+
         if lives == 0:
             result = 'lose'
             return True
@@ -74,6 +78,7 @@ def check_game(guessed_letter, secret_word, display):
     if '_' not in display:
         result = 'win'
         return True
+
     return False
 
 
@@ -119,12 +124,15 @@ def display_game_over_screen(result):
     """
     clear_screen()
     print("\n" + "*" * 60)
+
     if result == "win":
-        print("*{:^58}*".format("Congratulations! You won!"))
-    elif result == "lose":
-        print("*{:^58}*".format("Game over! You lost!"))
+        print("*{:^67}*".format("\033[92mCongratulations! You won!\033[0m"))
+
+    if result == "lose":
+        print("*{:^67}*".format("\033[91mGame over! You lost!\033[0m"))
+
     print(hangman_picture.lives_left[lives])
-    print("*{:^58}*".format(f"Your word was {secret_word}"))
+    print("*{:^67}*".format(f"Your word was \033[33m{secret_word}\033[0m"))
     print("*" * 60)
     print("*{:^58}*".format("Do you want to..."))
     print("*{:^58}*".format("1. Play Again?"))
@@ -137,6 +145,7 @@ def restart_game():
     Resets the game variables for a new game.
     """
     global secret_word, display, lives, game_over, guessed_letter, already_used
+
     secret_word = random.choice(word_list)
     display = create_display(secret_word)
     lives = 6
@@ -154,26 +163,32 @@ def clear_screen():
 
 
 if __name__ == "__main__":
+
     while True:
         display_home_screen()
         choice = input("Enter your choice:\n")
 
         if choice == "1":
             restart_game()
+
             while not game_over:
                 clear_screen()
                 display_ingame_screen()
                 guessed_letter = guess_letter()
+
                 while guessed_letter is False:
                     guessed_letter = guess_letter()
+
                 display = update_display(secret_word, guessed_letter, display)
                 print(display)
                 game_over = check_game(guessed_letter, secret_word, display)
 
                 if game_over:
+
                     while True:
                         display_game_over_screen(result)
                         choice = input("Enter your choice: ")
+
                         if choice == "1":
                             restart_game()
                             break
@@ -181,12 +196,13 @@ if __name__ == "__main__":
                             clear_screen()
                             break
                         else:
-                            print("Invalid choice. Please try again.\n")
+                            print("\033[91mInvalid choice.")
+                            print("Please try again.\033[0m\n")
                 continue
 
         elif choice == "2":
             break
 
         else:
-            print("Invalid choice. Please try again.\n")
+            print("\033[91mInvalid choice. Please try again.\033[0m\n")
             clear_screen()
